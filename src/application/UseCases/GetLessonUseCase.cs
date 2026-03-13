c81a335b9225345eeeb78b92a309206f1991ad53
Lucas Aguiar <7lucasdaniel@gmail.com>
2026-03-13 17:57:54 -0300

    refactor: Exclude question details and last answers from `GetLessonUseCase` and `LessonDtoOut`.

--- src/application/UseCases/GetLessonUseCase.cs
+++ src/application/UseCases/GetLessonUseCase.cs
@@ -6,28 +6,12 @@ using Domain.Interfaces.Services;
 
 namespace Application.UseCases
 {
-    public class GetLessonUseCase(ILessonRepository lessonRepository, ILessonService lessonService, IAnswerService answerService)
+    public class GetLessonUseCase(ILessonRepository lessonRepository, ILessonService lessonService)
     {
         public async Task<UseCaseResult<LessonDtoOut>> ExecuteAsync(Guid publicLessonId, Guid publicUserId)
         {
             var lessonFromDb = await lessonRepository.GetLesson(publicLessonId);
             if (lessonFromDb is null) return new() { StatusCode = HttpStatusCode.NoContent };
-
-            var questions = lessonFromDb.Questions
-                .Select(q => new QuestionDtoOut
-                {
-                    PublicId = q.PublicId,
-                    Statement = q.Statement,
-                    Alternatives = q.Alternatives
-                    .Select(a => new AlternativeDtoOut
-                    {
-                        PublicId = a.PublicId,
-                        QuestionId = a.QuestionId,
-                        Text = a.Text
-                    })
-                    .ToList(),
-                })
-                .ToList();
 
             return new()
             {
@@ -37,8 +21,6 @@ namespace Application.UseCases
                     Description = lessonFromDb.Description,
                     Title = lessonFromDb.Title,
                     VideoUrl = lessonFromDb.VideoUrl,
-                    Questions = questions,
-                    GetLastAnswersOut = await answerService.GetLastAnswersAsync(publicUserId, publicLessonId),
                     Concluded = await lessonService.LessonAreAlreadyAnswered(publicUserId, lessonFromDb.PublicId),
                 },
             };
--- src/domain/Dtos/LessonDtoOut.cs
+++ src/domain/Dtos/LessonDtoOut.cs
@@ -7,7 +7,5 @@ namespace Domain.Dtos
         public string? Description { get; set; }
         public string? VideoUrl { get; set; }
         public bool Concluded { get; set; }
-        public GetLastAnswersOut GetLastAnswersOut { get; set; } = new();
-        public ICollection<QuestionDtoOut> Questions { get; set; } = [];
     }
 }
