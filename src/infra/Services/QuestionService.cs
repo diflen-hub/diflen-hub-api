11b4cb6a4c459c072ba32737fa5c19245120c41f
Lucas Aguiar <7lucasdaniel@gmail.com>
2026-05-01 23:11:46 -0300

    [master] 🐞 fix: corrige bug de verificação de respostas

--- src/infra/Services/QuestionService.cs
+++ src/infra/Services/QuestionService.cs
@@ -8,7 +8,11 @@ namespace Infra.Services
         public async Task<bool> WasUnityCorrectlyAnswered(Guid publicUnityId, Guid publicUserId)
         {
             var unityQuestions = await questionRepository.GetListAsync(q => q.Unity.PublicId == publicUnityId);
-            var userAnswers = await answerRepository.GetListAsync(a => a.Unity.PublicId == publicUnityId && a.User.PublicId == publicUserId);
+            var userAnswers = await answerRepository.GetListAsync(a => 
+                a.Unity.PublicId == publicUnityId &&
+                a.User.PublicId == publicUserId &&
+                a.IsCorrect
+            );
 
             if (unityQuestions.Count == 0) return false;
             return userAnswers.Count == unityQuestions.Count;
